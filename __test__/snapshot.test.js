88b594671ec6b2ac31eec354e955893ef3224dbc
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-07-22 07:46:44 -0500

    Excepciones Snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -7,4 +7,23 @@ describe("Es hora de las intantaneas", () => {
 
     /** jest -u => actualiza los snapshots */
   });
+  test("Siempre fallara la instantanea", () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    };
+
+    // expect(user).toMatchSnapshot();
+  });
+
+  test("Tenemos una excepción dentro del codigó", () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      name: "Jorge Rangel",
+    };
+
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number),
+    });
+  });
 });
